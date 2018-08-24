52ef7eb76e3fbd710113916a048c6c1a74e6b4d4
Grace Hartzell <gracehartzell@utexas.edu>
2018-08-24 12:50:36 -0500

    Update learn.js

--- js/learn.js
+++ js/learn.js
@@ -1,43 +1,36 @@
-const results = [];
-
 const resultsList = document.getElementsByClassName('cards')[0];
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 
+function onSearch(ev) {
+  let results = [];
+  resultsList.innerHTML = ''
+  ev.preventDefault();
+    
+const herpName = searchInput.value;
+  fetch(`https://api.inaturalist.org/v1/search?q=${herpName}`)
+    .then(response => response.json())
+    .then((data) => {
+      const taxonFilterApplied = data.results.filter(obj => obj.type === 'Taxon');
+        
+        for (i = 0; i < taxonFilterApplied.length; i++) {
+            const currentItem = taxonFilterApplied[i].record
+            const herpObj = {
+                commonName:currentItem.preferred_common_name,
+                sciName:currentItem.name,
+                observCount:currentItem.observations_count,
+                extinct:currentItem.extinct,
+                photo:currentItem.taxon_photos[0].photo.medium_url,
+                wiki:currentItem.wikipedia_url
+            };
+            results.push(herpObj)
+        }
 
-searchButton.addEventListener('keyup', (ev) => {
-    if (ev.keyCode === 13) {
-        console.log(`hi`)
-        searchButton.click();
-    }
-});
-
-searchButton.addEventListener('click', (ev) => {
-    ev.preventDefault();
-    const herpName = searchInput.value;
-
-    fetch(`https://api.inaturalist.org/v1/search?q=${herpName}`)
-        .then(response => response.json())
-        .then((data) => {
-            const taxonFilter = data.results.filter(obj => obj.type === 'Taxon');
-            
-            for (i = 0; i < taxonFilter.length; i++) {
-                const currentItem = taxonFilter[i].record
-                const herpObj = {
-                    commonName:currentItem.preferred_common_name,
-                    sciName:currentItem.name,
-                    observCount:currentItem.observations_count,
-                    extinct:currentItem.extinct,
-                    photo:currentItem.taxon_photos[0].photo.medium_url,
-                    wiki:currentItem.wikipedia_url
-                };
-                results.push(herpObj)
-            }
-
-
+        
     resultsList.innerHTML = results.map(createHerpCard).join('');
-            
+    searchInput.value = '';
             function createHerpCard(herpObj) {
+                
                 let extinctType = (herpObj.extinct) ? `` : `not`;
                 return `
                         <div class="id-card-wrapper">
@@ -51,7 +44,7 @@ searchButton.addEventListener('click', (ev) => {
                                         <br>
                                             <p>Scientific Name: ${herpObj.sciName}</p>
                                             <br>
-                                            <p>Observation Count: ${herpObj.observCount}</p>
+                                            <p>Observation Count in Database: ${herpObj.observCount}</p>
                                             <br>
                                             <p>This species is ${extinctType} extinct.</p>
                                             <br>
@@ -63,4 +56,18 @@ searchButton.addEventListener('click', (ev) => {
                 `
             };
         });
-    })
+    
+}
+
+searchInput.addEventListener('keypress', (ev) => {
+  if (ev.keyCode === 13) {
+    ev.preventDefault();
+    return onSearch(ev);
+  } else {
+    return null
+  }
+});
+
+searchButton.addEventListener('click', (ev) => {
+  onSearch(ev);
+});
